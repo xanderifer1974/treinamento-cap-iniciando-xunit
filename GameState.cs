be8bbb814f38cd3eda764a36a1bc377151b68e3f
Alexandre Ribeiro Ferreira <alexandre.b.ferreira@capgemini.com>
2022-05-19 16:21:20 -0300

    Criação da classe GameState

---
+++ GameState.cs
@@ -0,0 +1,39 @@
+﻿using IniciandoXUnit;
+using System;
+using System.Collections.Generic;
+
+namespace GameEngine
+{
+    public class GameState
+    {
+        public static readonly int EarthquakeDamage = 25;
+
+        public List<PlayerCharacter> Players { get;  set; } = new List<PlayerCharacter>();
+
+        public Guid Id { get; set; } = Guid.NewGuid();
+
+        public GameState()
+        {
+            CreateGameWorld();
+        }
+
+        public void Earthquake()
+        {
+            foreach(var player in Players)
+            {
+                player.TakeDamage(EarthquakeDamage);
+                
+            }
+        }
+
+        public void Reset()
+        {
+            Players.Clear();
+        }
+
+        public void CreateGameWorld()
+        {
+            System.Threading.Thread.Sleep(2000);
+        }  
+    }
+}
